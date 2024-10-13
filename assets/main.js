3750338738863eb212fe948737d5c529617c52f3
chickencoc <solutw@gmail.com>
2024-10-13 19:50:01 +0900

    draw game & animation

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,61 @@
-document.getElementById("ball").addEventListener("click", () => {
-    anime({
-        targets: '#ball',
-        translateY: -250,
+let isDone = false;
+const txtEl = document.getElementById('ball-txt');
+
+const ballAnime = anime({
+    targets: '#ball',
+    autoplay: false,
+    rotateX: -10,
+    rotateY: 10,
+    translateY: {
+        value: -325,
         duration: 700,
-        easing: 'easeInOutQuad'
-    });
+        easing:	'easeInOutBack'
+    },
+    scale: {
+        value: 2.5,
+        duration: 550,
+        delay: 300,
+        easing: 'easeInOutQuart'
+    },
+    width: {
+        value: "125px",
+        duration: 500,
+        delay: 500,
+        easing: 'easeInOutQuart'
+    },
+    borderRadius: {
+        value: '5px',
+        duration: 700,
+        delay: 300,
+        easing: 'easeInOutQuart'
+    }
+});
+const cubeAnime = anime({
+    targets: '#cube',
+    autoplay: false,
+    rotateX: [-10, -10],
+    rotateY: {
+        value: [-10, -1810],
+        duration: 1000
+    },
+    easing: 'easeOutQuart'
 })
+
+console.log(cubeAnime)
+
+function getResult() {
+    if(isDone) return;
+    else isDone = true;
+
+    cubeAnime.play();
+    cubeAnime.complete = () => ballAnime.play();
+    ballAnime.complete = () => {
+        txtEl.style.display = 'inherit';
+    }
+}
+
+function resetGame() {
+    isDone = false;
+    ballAnime.reset();
+    txtEl.style.display = 'none';
+}

5d4e5b08d7cd4ae22c6e43ea4ff3d8d187c56a1a
chickencoc <solutw@gmail.com>
2024-10-21 05:08:07 +0900

    finish control and drawInBox page, upload sample data

--- assets/main.js
+++ assets/main.js
@@ -1,60 +1,86 @@
 let isDone = false;
 const txtEl = document.getElementById('ball-txt');
 
+const cubeAnime = anime({
+    targets: '#cube',
+    autoplay: false,
+    rotateX: [-10, -2],
+    rotateY: {
+        value: [-10, -1800],
+        duration: 1000
+    },
+    easing: 'easeInBack'
+});
 const ballAnime = anime({
     targets: '#ball',
     autoplay: false,
-    rotateX: -10,
-    rotateY: 10,
+    rotateX: -2,
     translateY: {
         value: -325,
         duration: 700,
-        easing:	'easeInOutBack'
+        easing:	'easeOutElastic'
     },
     scale: {
         value: 2.5,
         duration: 550,
-        delay: 300,
+        delay: 200,
         easing: 'easeInOutQuart'
     },
     width: {
         value: "125px",
         duration: 500,
-        delay: 500,
+        delay: 350,
         easing: 'easeInOutQuart'
     },
     borderRadius: {
         value: '5px',
         duration: 700,
-        delay: 300,
-        easing: 'easeInOutQuart'
+        delay: 400,
+        easing: 'easeOutQuart'
     }
 });
-const cubeAnime = anime({
-    targets: '#cube',
-    autoplay: false,
-    rotateX: [-10, -10],
-    rotateY: {
-        value: [-10, -1810],
-        duration: 1000
-    },
-    easing: 'easeOutQuart'
-})
 
+function returnGuest() {
+    // choose type
+    let list = [];
+    let rn = getRandomInt(0, 1000);
+    const ratio = getItem(guestChance);
+    console.log(rn)
+
+    if(ratio[0] >= rn) {
+        list = getGuestList('내부');
+    } else {
+        list = getGuestList('외부');
+    }
+
+    rn = getRandomInt(0, list.length - 1)
+
+    return list[rn];
+}
 
 function getResult() {
-    if(isDone) return;
-    else isDone = true;
+    try {
+        if(isDone) return;
+        else isDone = true;
 
-    cubeAnime.play();
-    cubeAnime.complete = () => ballAnime.play();
-    ballAnime.complete = () => {
-        txtEl.style.display = 'inherit';
+        const gusetObj = returnGuest();
+        txtEl.innerText = gusetObj.name;
+        console.log(gusetObj)
+
+        cubeAnime.play();
+        cubeAnime.complete = () => ballAnime.play();
+        setTimeout(() => {
+            txtEl.style.display = 'inherit';
+        }, 1875);
+    }
+    catch (e) {
+        alert('사람 목록 없음')
     }
 }
 
 function resetGame() {
     isDone = false;
     ballAnime.reset();
+    cubeAnime.reset();
     txtEl.style.display = 'none';
 }
--- assets/makeList.js
+++ assets/makeList.js
@@ -1,22 +1,23 @@
 const guestList = "guestList"; // 사람 목록
-const exceptList = "exceptList";  // 사람 제외 목록
+const originGuestList = "originGuestList";  // 원본 사람 목록
 const giftList = "giftList";  // 선물 목록
-const selectGuest = "selectGuest"; // 사용될 사람 목록
-const selectGift = "selectGift"; // 사용될 선물 목록
+const originGiftList = "originGiftList";  // 원본 선물 목록
 const guestChance = "guestChance"; // 사용될 사람 확률
 const giftChance = "giftChance"; // 사용될 선물 확률
 
 function nvl(i) {
     return i == undefined || i == "" ? undefined : i;
 }
-
+/**
+ * 랜덤 숫자 반환 함수
+ */
 function getRandomInt(min, max) {
     // Create byte array and fill with 1 random number
-    var byteArray = new Uint8Array(1);
+    var byteArray = new Uint16Array(1);
     window.crypto.getRandomValues(byteArray);
 
     var range = max - min + 1;
-    var max_range = 256;
+    var max_range = 65536;
 
     if (byteArray[0] >= Math.floor(max_range / range) * range)
         return getRandomInt(min, max);
@@ -25,7 +26,8 @@ function getRandomInt(min, max) {
 
 function getItem(key) {
     const json = localStorage.getItem(key);
-    return new Array(JSON.parse(nvl(json)));
+    const item = nvl(json);
+    return item && JSON.parse(item);
 }
 
 function setItem(key, value) {
@@ -33,52 +35,25 @@ function setItem(key, value) {
     return value;
 }
 
-// 등록
-function confirm(params) {
-    // const a = document.getElementById("listform")
-    // const fd = new FormData(a)
-    
-    // for(const x of fd.entries()) {
-    //     console.log(x)
-    // }
-    
-    const obj = { type: 'in', group: 'none', name: "kim"}
-
-    const list = [
-        { type: 'in', group: 'none', name: "kim"},
-        { type: 'in', group: 'none', name: "lee"},
-        { type: 'out', group: 'none', name: "park"},
-        { type: 'in', group: 'kim1', name: "kim1"},
-        { type: 'in', group: 'kim1', name: "lee1"},
-        { type: 'in', group: 'kim1', name: "park1"},
-    ]
-}
-
-function getGuestList(type, group, name) {
+function getGuestList(type) {
     let list = getItem(guestList);
     if(nvl(type)) {
-        list.filter(obj => obj.type == type)
+        list = list.filter(obj => obj.type == type)
     }
-    if(nvl(group)) {
-        list.filter(obj => obj.group == group)
-    }
-    if(nvl(name)) {
-        list.filter(obj => obj.name == name)
-    }
-
+    console.log(list)
     return list
 }
 
-function getGiftList(prefer, price, name) {
+function getGiftList(price, prefer, name) {
     let list = getItem(giftList);
-    if(nvl(prefer)) {
-        list.filter(obj => obj.prefer >= prefer.min && obj.prefer <= prefer.max)
-    }
     if(nvl(price)) {
-        list.filter(obj => obj.price >= price.min && obj.price <= price.max)
+        list = list.filter(obj => obj.type >= price.min && obj.type <= price.max)
+    }
+    if(nvl(prefer)) {
+        list = list.filter(obj => obj.group >= prefer.min && obj.group <= prefer.max)
     }
     if(nvl(name)) {
-        list.filter(obj => obj.name == name)
+        list = list.filter(obj => obj.name == name)
     }
 
     return list
@@ -89,10 +64,6 @@ function getGiftList(prefer, price, name) {
 
 
 // ------------------
-
-function sampleGet() {
-    document.querySelector("div.front").innerText= getItem(guestList.toString())
-}
 
 function sampleGuest() {
 
@@ -105,29 +76,26 @@ function sampleGuest() {
         { type: 'in', group: 'kim1', name: "park1"},
     ]
 
-    setItem(guestList, JSON.stringify(list))
+    setItem(guestList, list)
 }
 
 function sampleGift() {
 
     const list = [
-        { prefer: '9', price: 400000, name: "gift1"},
-        { prefer: '8', price: 45000, name: "gift2"},
-        { prefer: '8', price: 30000, name: "gift3"},
-        { prefer: '7', price: 125000, name: "gift4"},
-        { prefer: '5', price: 140000, name: "gift5"},
-        { prefer: '2', price: 60000, name: "gift6"},
-        { prefer: '1', price: 550000, name: "gift7"},
-        { prefer: '0', price: 10000, name: "gift8"},
+        { type: 400000, group: '9', name: "gift1"},
+        { type: 45000, group: '8', name: "gift2"},
+        { type: 30000, group: '8', name: "gift3"},
+        { type: 125000, group: '7', name: "gift4"},
+        { type: 140000, group: '5', name: "gift5"},
+        { type: 60000, group: '2', name: "gift6"},
+        { type: 550000, group: '1', name: "gift7"},
+        { type: 10000, group: '0', name: "gift8"},
     ]
 
-    setItem(giftList, JSON.stringify(list))
+    setItem(giftList, list)
 }
 
 function sampleGuestRatio() {
     const ratio = {t: 10000, a: 100, b: 494.06, c: 4549.50, d: 329.70, e: 2617.82, f: 121.78, g: 990.10, h: 797.03}
 }
-
-sampleGuest()
-sampleGift()
 
